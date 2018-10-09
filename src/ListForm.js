6b64cd3eda308f3816049e2bd1d1cfa2e0929fa0
dwyfrequency <jdwy215@gmail.com>
2018-10-08 21:27:46 -0400

    adding more css to fform

--- src/ListForm.js
+++ src/ListForm.js
@@ -14,30 +14,21 @@ class ListForm extends Component {
     const { gratitudeEntry } = this.state;
     return (
       <form>
-        <div className="form-group row">
-          <label htmlFor="gratitudeEntry" className="col-sm-2 col-form-label">
-            Gratitude Entry
-          </label>
-          <div className="col-sm-8">
-            <input
-              type="text"
-              name="gratitudeEntry"
-              className="form-control"
-              value={this.state.gratitudeEntry}
-              onChange={this.onFieldChange}
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <div className="col-sm-10">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={e => this.props.onClick(e, gratitudeEntry)}
-            >
-              Add
-            </button>
-          </div>
+        <div className="AddGratitudeForm">
+          <label htmlFor="gratitudeEntry">Gratitude Entry</label>
+          <input
+            type="text"
+            name="gratitudeEntry"
+            value={this.state.gratitudeEntry}
+            onChange={this.onFieldChange}
+          />
+          <input
+            type="button"
+            value="Add"
+            onClick={e => this.props.onClick(e, gratitudeEntry)}
+            className="btn btn-primary"
+            id="btn-grat"
+          />
         </div>
       </form>
     );
